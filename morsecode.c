d54fa9abe72b1f57d725a375eacba8f6f6f3cd45
Aditya Bawankule <adityabawankule@gmail.com>
2017-01-26 14:33:33 -0600

    Create morsecode.c

---
+++ morsecode.c
@@ -0,0 +1,151 @@
+/*
+This program simply blinks the word you enter in the string
+
+Jan 25 17
+*/
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(1, OUTPUT); //LED on Model A   
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // turn the LED on (HIGH is the voltage level)
+  int basetime = 200;
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(3*basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(3*basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(3*basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(3*basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(3*basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(7*basetime);               // wait for a second
+}
+void dot(){
+  digitalWrite(1, HIGH);
+  delay(basetime);               // wait for a second
+  // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+  delay(basetime);               // wait for a second
+}
+void dash(){
+  
+}
+void a(){
+  
+}
+void b(){
+  
+}
+void c(){
+  
+}
+void d(){
+  
+}
+void e(){
+  
+}
+void f(){
+  
+}
+void g(){
+  
+}
+void h(){
+  
+}
+void i(){
+  
+}
+void j(){
+  
+}
+void k(){
+  
+}
+void l(){
+  
+}
+void m(){
+  
+}
+void n(){
+  
+}
+void o(){
+  
+}
+void p(){
+  
+}
+void q(){
+  
+}
+void r(){
+  
+}
+void s(){
+  
+}
+void t(){
+  
+}
+void u(){
+  
+}
+void v(){
+  
+}
+void w(){
+  
+}
+void x(){
+  
+}
+void y(){
+  
+}
+void z(){
+  
+}
+
