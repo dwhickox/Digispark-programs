cad0eb8ae9d7fe879d6733dc755a634a9845f895
dwhickox <dwhickox@users.noreply.github.com>
2017-02-17 08:43:25 -0600

    This is broken, this is just the framework

---
+++ MorseCode.ino
@@ -0,0 +1,42 @@
+/*
+ This program simply blinks the word you enter in the string
+ Copyright David Hickox
+ Jan 25 17
+ */
+ // the setup routine runs once when you press reset:
+int basetime = 200;
+
+void setup() {                
+   // initialize the digital pin as an output.
+   pinMode(1, OUTPUT); //LED on Model A   
+   
+ }
+ 
+ // the loop routine runs over and over again forever:
+ void loop() {
+   // turn the LED on (HIGH is the voltage level)
+   digitalWrite(1, HIGH);
+   delay(basetime);               // wait for a second
+   // turn the LED off by making the voltage LOW
+   digitalWrite(1, LOW); 
+   delay(basetime);               // wait for a second
+ }
+ 
+ void a() {
+ 
+} 
+ void b() {
+ 
+} 
+ void c() {
+ 
+}
+ void d() {
+ 
+}
+ void e() {
+ 
+}
+ void f() {
+ 
+}
